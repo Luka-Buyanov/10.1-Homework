761ceadfe3dd7a88f96757338ab0f385c376f389
Luka Buyanov <luka.buanov@gmail.com>
2024-09-01 13:48:50 +0300

    В модуле 'processing' создана функция 'sort_by_date'

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+from src.widget import get_date
+
+
 def filter_by_state(dictionary_: list, state_: str = "EXECUTED") -> list:
     """Функция сортирующая список по критерию 'state'"""
 
@@ -9,3 +12,16 @@ def filter_by_state(dictionary_: list, state_: str = "EXECUTED") -> list:
             continue
 
     return sorted_dictionary
+
+
+def sort_by_date(date_dictionary_: list, date_: bool = False) -> list:
+    """Функция сортирующая список по дате"""
+
+    def get_date_(dictionary: dict) -> str:
+        """Подфункция выводящая дату в привычном формате"""
+
+        good_date = get_date(dictionary["date"])
+        return good_date
+
+    sorted_date = sorted(date_dictionary_, key=get_date_, reverse=date_)
+    return sorted_date
